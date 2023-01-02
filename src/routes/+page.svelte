218f1d537056a772ea5e6133f3a4f822d8563e9f
LanaYoobee <270164718@yoobeestudent.ac.nz>
2023-01-02 16:58:02 +1300

    tidy up project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,39 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+import { page } from "$app/stores";
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Advent of Code</title>
+	<meta name="description" content="Advent of Code exercises" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+<div class="text-column">
+	<h1>Advent of Code 2022</h1>
 
-		to your new<br />SvelteKit app
-	</h1>
+	<p>
+		This is a <a href="https://adventofcode.com/2022">Advent of Code</a> results tab.
+	</p>
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+	<nav>
+		<ul>
+			<li aria-current={$page.url.pathname === "/day1" ? "page" : undefined}>
+				<a href="/day1">Day 1</a>
+			</li>
+			<li aria-current={$page.url.pathname === "/day2" ? "page" : undefined}>
+				<a href="/day2">Day 2</a>
+			</li>
+			<li aria-current={$page.url.pathname === "/day3" ? "page" : undefined}>
+				<a href="/day3">Day 3</a>
+			</li>
+			<li aria-current={$page.url.pathname === "/day4" ? "page" : undefined}>
+				<a href="/day4">Day 4</a>
+			</li>
+			<li aria-current={$page.url.pathname === "/day5" ? "page" : undefined}>
+				<a href="/day5">Day 5</a>
+			</li>
+			<li aria-current={$page.url.pathname === "/day6" ? "page" : undefined}>
+				<a href="/day6">Day 6</a>
+			</li>
+		</ul>
+	</nav>
+</div>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -16,23 +16,8 @@ import github from "$lib/images/github.svg";
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
 		</svg>
 		<ul>
-			<li aria-current={$page.url.pathname === "/" ? "page" : undefined}>
-				<a href="/">Home</a>
-			</li>
-			<li aria-current={$page.url.pathname === "/about" ? "page" : undefined}>
-				<a href="/about">About</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith("/sverdle") ? "page" : undefined}>
-				<a href="/sverdle">Sverdle</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith("/json") ? "page" : undefined}>
-				<a href="/jsontutorial">JSON Tutorial</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith("/adventofcode") ? "page" : undefined}>
-				<a href="/adventofcode">Advent of Code</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith("/testing") ? "page" : undefined}>
-				<a href="/testing">Testing</a>
+			<li aria-current={$page.url.pathname.startsWith("/") ? "page" : undefined}>
+				<a href="/">Advent of Code</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">

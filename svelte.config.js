a52be317b563bf35b28f92e274ec48130724d3af
LanaYoobee <101915669+LanaYoobee@users.noreply.github.com>
2024-11-12 10:10:08 +1300

    update config

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,9 @@
 import adapter from '@sveltejs/adapter-auto';
-import {sveltePreprocess} from 'svelte-preprocess';
-
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: sveltePreprocess(),
-
 	kit: {
-		adapter: adapter({
-		}),
-
+		adapter: adapter()
 	}
 };
 
